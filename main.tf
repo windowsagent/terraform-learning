cc058d6c498e7733c77c848733fdee724917699a
windowsagent <juanjphc@gmail.com>
2024-03-25 20:19:07 -0600

    Add 2 EC2 instances

--- main.tf
+++ main.tf
@@ -6,3 +6,15 @@ resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 }
+
+
+resource "aws_instance" "web" {
+  count         = 2
+  ami           = "ami-abcdef01"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main.id
+
+  tags = {
+    Name = "web-server-${count.index}"
+  }
+}
